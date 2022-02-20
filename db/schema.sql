d0c0aaa7e33b1ca820bdddada96232168b584a93
Courtney <courtneymkirk@hotmail.com>
2022-02-20 12:51:02 -0500

    updating schema tables

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+        id INTEGER AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR (30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES deaprtment(id) ON DELETE SET NULL
+);
+
