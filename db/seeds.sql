41d312bf95702285ad124f6840bcdba304009ea5
Courtney <courtneymkirk@hotmail.com>
2022-02-20 13:27:37 -0500

    add values to seeds

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (name)
+VALUES
+    ('Executive'),
+    ('Human Resources');
+    ('Sales'),
+    ('Marketing');
+
+    INSERT INTO role (title, salary, department_id)
+    VALUES
+     ('President/CEO', '250000', 1),
+     ('Director of HR', '130000', 2),
+     ('HR Coordinator', '75000', 2);
+     ('Director of Sales', '175000', 3),
+     ('Sales Manager', '100000', 3),
+     ('Salesperson', '80000', 3),
+     ('Director of Marketing', '125000', 4),
+     ('Marketing Coordinator', '85000', 4),
+     ('Marketing Intern', '30000', 4),
