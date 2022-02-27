87c663bd0b53ffa2d233afa76bc67045fd83fd90
Courtney <courtneymkirk@hotmail.com>
2022-02-27 11:54:20 -0500

    removed code not needed

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ const addEmployeeQuestions = [
             console.table(results);
           });
           startMenu();
-          break;
+    
         
         case "Add a Role":
           db.query('SELECT * FROM department', function (err, results) {
@@ -165,7 +165,7 @@ const addEmployeeQuestions = [
             console.table(results);
           });
           addRole();
-          break;
+          
   
         case "Show All Departments":
           db.query('SELECT * FROM department', function (err, results) {
@@ -173,11 +173,11 @@ const addEmployeeQuestions = [
             console.table(results);
           });
           startMenu();
-          break;
+          
   
         case "Add a Department":
           addDepartment();
-          break;
+        
   
         case "Show All Employees":
           db.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;", function (err, results) {
@@ -185,7 +185,7 @@ const addEmployeeQuestions = [
             console.table(results);
           });
           startMenu();
-          break;
+          
   
         case "Add an Employee":
           db.query('SELECT role.*, department.name AS department_name FROM role LEFT JOIN department ON role.department_id = department.id', function (err, results) {
@@ -197,7 +197,7 @@ const addEmployeeQuestions = [
             console.table(results);
           });
           addEmployee();
-          break;
+          
   
         case "Update an Employee's Role":
           db.query('SELECT employee.id, employee.first_name, employee.last_name FROM employee', function (err, results) {
@@ -205,7 +205,7 @@ const addEmployeeQuestions = [
             console.table(results);
           });
           chooseEmployee();
-          break;
+          
       }
     });
   }
