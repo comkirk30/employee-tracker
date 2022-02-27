b9b7ee2879e23bfbe4c9dc6bb3167ecd748dbe7c
Courtney <courtneymkirk@hotmail.com>
2022-02-27 17:00:50 -0500

    updated seeds syntax due to error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO department(name)
 VALUES
     ('Executive'),
-    ('Human Resources');
+    ('Human Resources'),
     ('Sales'),
     ('Marketing'),
     ('Lifestyle');
@@ -10,7 +10,7 @@ INSERT INTO role(title, salary, department_id)
 VALUES
      ('President/CEO', '250000', 1),
      ('Director of HR', '130000', 2),
-     ('HR Coordinator', '75000', 2);
+     ('HR Coordinator', '75000', 2),
      ('Director of Sales', '175000', 3),
      ('Sales Manager', '100000', 3),
      ('Salesperson', '80000', 3),
